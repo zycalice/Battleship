d82163d49d2873337e190fd988c8518bcc9fbf12
zycalice <alicezhang727@gmail.com>
2020-12-23 14:00:47 -0500

    add constructor

--- src/OceanTest.java
+++ src/OceanTest.java
@@ -19,6 +19,21 @@ class OceanTest {
     }
 
     @org.junit.jupiter.api.Test
+    void constructorTest(){
+        Ocean oceanInit = new Ocean();
+        //test the shape of the ship[][]
+        assertEquals(10, oceanInit.getShipArray()[0].length);
+        assertEquals(10, oceanInit.getShipArray().length);
+
+        //test each ship's type, should be empty
+        for (int i=0; i<10;i++){
+            for (int j=0; j<10;j++) {
+                assertEquals("empty", oceanInit.getShipArray()[i][j].getShipType());
+            }
+        }
+    }
+
+    @org.junit.jupiter.api.Test
     void isOccupiedFalseTest() {
         assertFalse(ocean.isOccupied(3,3));
     }
@@ -72,33 +87,43 @@ class OceanTest {
 
     @org.junit.jupiter.api.Test
     void getHitCountTest() {
+        //shoot at a few places
         ocean.shootAt(5,5);
         ocean.shootAt(3,4);
+        //test gets shots fired
         assertEquals(2, ocean.getShotsFired());
     }
 
     @org.junit.jupiter.api.Test
     void getShipsSunkTest() {
+        //before shooting
         assertEquals(0, ocean.getShipsSunk());
+        //shoot at a submarine
         ocean.shootAt(5,5);
+        //after shooting
         assertEquals(1, ocean.getShipsSunk());
     }
 
     @org.junit.jupiter.api.Test
     void isGameOverYesTest() {
+        //set number of ships sunk to 10
         ocean.shipsSunk = 10;
+        //check if the game is over after setting that
         assertTrue(ocean.isGameOver());
     }
 
     @org.junit.jupiter.api.Test
     void isGameOverNoTest() {
+        //set number of ships sunk to 5
         ocean.shipsSunk = 5;
+        //check if the game is over after setting that
         assertFalse(ocean.isGameOver());
     }
 
     @org.junit.jupiter.api.Test
     void getShipArrayTest() {
         Ship[][] shipsTest = ocean.getShipArray();
+        //check a few positions according to the setup
         assertEquals("empty", shipsTest[1][1].getShipType());
         assertEquals("destroyer", shipsTest[3][4].getShipType());
         assertEquals("destroyer", shipsTest[3][5].getShipType());
@@ -108,7 +133,9 @@ class OceanTest {
     @Test
     void initiateShipTest(){
         ArrayList<Ship> ships = ocean.initiateShips();
+        //test size
         assertEquals(10, ships.size());
+        //select a few positions for testing
         assertEquals("battleship", ships.get(0).getShipType());
         assertEquals("destroyer", ships.get(5).getShipType());
         assertEquals("submarine", ships.get(9).getShipType());
